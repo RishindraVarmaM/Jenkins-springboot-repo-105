362fc02e0302f6c1667146edb2027f53250f1b8e
Rishi <2300031875@gmail.com>
2025-08-12 18:12:17 +0530

    updated pom.xml

--- src/main/java/com/klef/SpringBootDemoProjectApplication.java
+++ src/main/java/com/klef/SpringBootDemoProjectApplication.java
@@ -2,16 +2,19 @@ package com.klef;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootDemoProjectApplication extends SpringBootServletInitializer
+public class SpringBootDemoProjectApplication extends SpringBootServletInitializer {
 
-{
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoProjectApplication.class, args);
+        System.out.println("Hello DevOps :) ");
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootDemoProjectApplication.class, args);
-		System.out.println("Hello DevOps :) ");
-	}
-
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SpringBootDemoProjectApplication.class);
+    }
 }
